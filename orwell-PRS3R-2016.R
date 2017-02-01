597493251d90fd1880e11891c0821d1bd326ac9f
vinay singh <vinaytweets@gmail.com>
2017-01-31 20:45:46 -0500

    changes to style

--- orwell-PRS3R-2016.R
+++ orwell-PRS3R-2016.R
@@ -1,61 +1,132 @@
 # setting up working directory
+
 setwd("~/Dropbox")
+
+# clear up memory
+
 rm(list=ls())
 
 # reading the .csv file into R
+
 orwell_original <- read.csv("Orwell-PRS3R-2016.csv", header = TRUE)
 
 # loading required package
+
 require(tidyverse)
 
-# subsetting by only selecting relevant variables
-orwell <- select(orwell_original, Posting.Date, Machine.Number, Grade, 
-                 Material, Order, Employee.Number, Shifts, Confirmed.Qty, 
-                 Scrap.Actual, Machining.Plan, Machining.Actual, Labor.Actual)
+# filtering only relevant variables from orwell_original dataset
+
+orwell <- select(
+  orwell_original, 
+  Posting.Date, 
+  Machine.Number, 
+  Grade, 
+  Material, 
+  Order, 
+  Employee.Number, 
+  Shifts, 
+  Confirmed.Qty, 
+  Scrap.Actual, 
+  Machining.Plan, 
+  Machining.Actual, 
+  Labor.Actual
+  )
 
 # changing column names to lower case and easy to read (I like that)
-colnames(orwell) <- c("date", "machine", "grade", "material", 
-                      "order", "employee", "shift", "qty", "scrap", 
-                      "machine_plan", "machine_actual", "labor_actual")
 
-# converting required variables to factor
+colnames(orwell) <- c(
+  "date", 
+  "machine", 
+  "grade", 
+  "material", 
+  "order", 
+  "employee", 
+  "shift", 
+  "qty", 
+  "scrap", 
+  "machine_plan", 
+  "machine_actual", 
+  "labor_actual")
 
-orwell$scrap <- as.numeric(orwell$scrap)
-orwell$qty <- as.numeric(orwell$qty)
-orwell$shift <- as.factor(orwell$shift)
-orwell$order <- as.factor(orwell$order)
+# converting required variables to factor or numeric and adding month variable
+
+orwell$scrap    <- as.numeric(orwell$scrap)
+orwell$qty      <- as.numeric(orwell$qty)
+orwell$shift    <- as.factor(orwell$shift)
+orwell$order    <- as.factor(orwell$order)
 orwell$material <- as.factor(orwell$material)
-orwell$date <- as.Date(orwell$date, format = "%d.%m.%Y")
-orwell$month <- months(orwell$date)
+orwell$date     <- as.Date(orwell$date, format = "%d.%m.%Y")
+orwell$month    <- months(orwell$date)
 orwell$monthnum <- as.factor(format(orwell$date, "%m"))
 orwell$weekdays <- as.factor(weekdays(orwell$date))
 
 # setting ordered factor for months and weeks
 
-orwell$month <- factor(orwell$month,levels=c(
-  "January","February","March","April","May","June", "July",
-  "August", "September", "October", "November", "December"),ordered=TRUE)
+orwell$month <- factor(
+  orwell$month,levels=c(
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June", 
+    "July",
+    "August", 
+    "September", 
+    "October", 
+    "November", 
+    "December"),
+  ordered=TRUE)
 
-orwell$weekdays <- factor(orwell$weekdays,levels=c(
-  "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
-  "Friday", "Saturday"),ordered=TRUE)
+orwell$weekdays <- factor(
+  orwell$weekdays,levels=c(
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday"),
+  ordered=TRUE)
 
-# s <- orwell %>% filter(machine != c(
-# '#', '002804', '005241', '0', '008449 E', '043101 V', '1271', 
-# '1459', '34283', 'INE: 005', '5241100', '7992', '8137', '8150', 
-# '8159', '8240', '8276', '8449', '8454', '8559', '90593'))
-
-# or should I just create a data frame containing 10 machine I need
-# which one is easier? I think second one
-# so lets get started
+# keeping required machines
 
 orwell <- filter(orwell, machine %in% c(
-  "28045", "32108", "43101", "50591", "50592", "50593", "50696", "52411", "52412"))
+  "28045", 
+  "32108", 
+  "43101", 
+  "50591", 
+  "50592", 
+  "50593", 
+  "50696", 
+  "52411", 
+  "52412")
+  )
 
 orwell <- filter(orwell, employee %in% c(
-  "20648", "20986", "21044", "21161", "21227", "21242",
-  "21269", "21361", "61301","63508", "63858", "63877", "63928", 
-  "64863", "64891", "65083"))
+  "20648", 
+  "20986", 
+  "21044", 
+  "21161", 
+  "21227", 
+  "21242",
+  "21269", 
+  "21361", 
+  "61301",
+  "63508", 
+  "63858", 
+  "63877", 
+  "63928", 
+  "64863", 
+  "64891", 
+  "65083")
+  )
+
+
+
+
+
+# tibble with total qty and scrap and grouping by key variables
 
 operator <- orwell %>%
   group_by(machine, monthnum, weekdays, shift, employee) %>%
@@ -64,6 +135,8 @@ operator <- orwell %>%
     total_scrap = sum(scrap, na.rm=TRUE)
   )
 
+# PLOT PLOT PLOT
+
 #ggplot(data=operator, mapping = aes(x = monthnum, y = total_qty)) + 
 #  geom_boxplot(mapping = aes(color = shift))
 
@@ -71,11 +144,16 @@ operator <- orwell %>%
 #  geom_boxplot() + facet_grid(machine~weekdays)
 
 # try to sumperimpose shift data over each other
-ggplot(data=operator, mapping = aes(x = monthnum, y = total_qty)) + 
-  geom_boxplot(mapping = aes(fill = shift), position="identity", alpha=0.40) + facet_grid(.~weekdays)
 
-# can we see how employee output changes over months? 
-# To make below work, we need a different tibble
+ggplot(data=operator, mapping = aes(x = monthnum, y = total_qty)) + 
+  geom_boxplot(mapping = aes(fill = shift), position="identity", alpha=0.40) + 
+  facet_grid(.~weekdays)
+
+
+
+
+
+# new tibble with grouping by month and employee
 
 op <- orwell %>%
   group_by(monthnum, employee) %>%
@@ -90,11 +168,18 @@ op <- orwell %>%
 # op$monthnum <- as.numeric(op$monthnum)
 
 ggplot(data=op, mapping = aes(x = monthnum, y = total_qty, color = employee)) + 
-  geom_point() + geom_smooth() + facet_wrap(~employee)
+  geom_point() + 
+  geom_smooth() + 
+  facet_wrap(~employee)
 
 # this plot is worth looking at 
 ggplot(data=op, mapping = aes(x = monthnum, y = total_qty)) + 
-  +   geom_point(aes(size = 0.5)) + facet_wrap(~employee)
+  geom_point(aes(size = 0.5)) + 
+  facet_wrap(~employee)
+
+
+
+
 
 # Now I want to focus on high quantity materials only
 # so first I'll create a tibble containing materials 
@@ -123,3 +208,8 @@ change <- orwell %>%
     total_machine_plan = sum(machine_plan, na.rm=TRUE),
     total_machine_actual = sum(machine_actual, na.rm=TRUE)
   )
+
+
+
+
+
